2b903320d83ba37b2a515359261b643ef1645009
Declimator <31232768+DCLMTR@users.noreply.github.com>
2022-03-03 16:59:00 -0800

    DoWhileDemo, TernaryOperatorDemo, ConsoleStore IP

--- src/_05_Intro_To_Ternary_Operator/TernaryOperatorDemo.java
+++ src/_05_Intro_To_Ternary_Operator/TernaryOperatorDemo.java
@@ -1,5 +1,7 @@
 package _05_Intro_To_Ternary_Operator;
 
+import java.util.Scanner;
+
 public class TernaryOperatorDemo {
 
     /*
@@ -39,9 +41,11 @@ public class TernaryOperatorDemo {
          * 4. Surround the previous parts with a do while loop and continue
          * until the response says "FIRE!".
          */
-        
-        
-        
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("What season is it?");
+        String str = keyboard.nextLine().equalsIgnoreCase("duck season") ? "FIRE!" : "rabbit season";
+        System.out.println(str);
+        keyboard.close();
         /*
          * WARNING: Ternary operators can be used to improve the readability of
          * your code if used sparingly for very simple if/else statements.
